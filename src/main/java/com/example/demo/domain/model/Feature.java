e3819a8191c07bd3640774377706293805fbae88
Eletrolucas <lucasvinnicius130.lv@gmail.com>
2024-07-09 12:23:40 -0300

    implementação da camada de dominio

---
+++ src/main/java/com/example/demo/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package com.example.demo.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "tb_feature")
+public abstract class Feature extends BaseItem {
+    
+    
+}
---
+++ src/main/java/com/example/demo/domain/model/News.java
@@ -0,0 +1,9 @@
+package com.example.demo.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "tb_news")
+public abstract class News extends BaseItem{
+
+    
+}
