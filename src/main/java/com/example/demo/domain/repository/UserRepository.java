2add395864a3a4ff17cc7070e5314e7eaffedac6
lucas <lucasvinnicius130.lv@gmail.com>
2024-07-09 21:59:17 -0300

    camada de servicos

--- src/main/java/com/example/demo/domain/repository/UserRepository.java
+++ src/main/java/com/example/demo/domain/repository/UserRepository.java
@@ -8,4 +8,6 @@ import com.example.demo.domain.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByAccountNumber(String accountNumber);
+
 }
---
+++ src/main/java/com/example/demo/domain/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.demo.domain.service.impl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.model.User;
+import com.example.demo.domain.repository.UserRepository;
+import com.example.demo.domain.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if(userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())){
+                throw new IllegalArgumentException("This user ID already exists.");
+        }
+        return this.userRepository.save(userToCreate);
+    }
+
+}
